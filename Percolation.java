402128c684821151f1559ec77cae7310307f311b
Andy Laughlin <andy.laughlin@airts.co.uk>
2020-02-15 13:37:18 +0000

    methods added to open a site, check if a site is open, find the index of a site

--- Percolation.java
+++ Percolation.java
@@ -1,74 +1,62 @@
-import java.util.ArrayList;
-
 public class Percolation {
 
-    public ArrayList<Site> grid;
+    public boolean[] grid;
+    public int gridDimension;
+    public int numberOfOpenSites;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
-        this.grid = new ArrayList<Site>();
-        this.createGrid(n);
+        this.gridDimension = n;
+        this.grid = this.createGrid();
+        this.numberOfOpenSites = 0;
     }
 
-    public void createGrid(int n) {
-
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < n + 1; j++) {
-                Site siteToAdd = new Site(i, j);
-                this.grid.add(siteToAdd);
-            }
-        }
+    private boolean[] createGrid() {
+        int gridSize = this.gridDimension * this.gridDimension;
+        return new boolean[gridSize];
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-        for (int i = 0; i < this.grid.size(); i++) {
-            Site site = this.grid.get(i);
-            if (site.row == row && site.column == col) {
-                site.isOpen = true;
-            }
-        }
+        int indexOfSiteToOpen = this.findSiteIndexFromRowAndColumn(row, col);
+        this.grid[indexOfSiteToOpen] = true;
+        this.numberOfOpenSites += 1;
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        for (int i = 0; i < this.grid.size(); i++) {
-            Site site = this.grid.get(i);
-            if (site.isOpen == true) {
-                return true;
-            }
-            else {
-                return false;
-            }
+        int indexOfSiteToCheck = this.findSiteIndexFromRowAndColumn(row, col);
+        if (this.grid[indexOfSiteToCheck] == true) {
+            return true;
         }
         return false;
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        for (int i = 0; i < this.grid.size(); i++) {
-            Site site = this.grid.get(i);
-            if (site.isFull == true) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-        return false;
+        
     }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
+        return this.numberOfOpenSites;
+    }
 
+    // Using the row and column, this method should give us the index position of that site
+    private int findSiteIndexFromRowAndColumn(int row, int col) {
+        int beginningIndexOfRow = this.gridDimension * (row - 1);
+        int siteIndex = beginningIndexOfRow += (col - 1);
+        return siteIndex;
     }
 
     // does the system percolate?
-    public boolean percolates()
+    // public boolean percolates()
 
     // test client (optional)
     public static void main(String[] args) {
         Percolation percolation = new Percolation(5);
-        System.out.println(percolation.grid);
+        percolation.open(2, 4);
+        boolean open = percolation.isOpen(2, 4);
+        System.out.println(open);
     }
 }
